752ff5d0534677c3df66e4405b9d74914fa69980
HellGateCorp <83019866+HellGateCorp@users.noreply.github.com>
2021-05-23 01:24:11 +0200

    Add files via upload

---
+++ CesarsFX-Cipher/src/cesarsfx/CesarsFX.java
@@ -0,0 +1,36 @@
+package cesarsfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CesarsFX extends Application{
+
+	public void start(Stage meineStage) throws Exception {
+		//eine Instanz von FXMLLoader erzeugen
+		FXMLLoader meinLoader = new FXMLLoader(getClass().getResource("cesar.fxml"));
+		//die Datei laden
+		Parent root = meinLoader.load();
+		//den Controller beschaffen
+		FXMLController meinController = meinLoader.getController();
+		//und die Bühne übergeben
+		meinController.setMeineStage(meineStage);
+		//die Szene erzeugen
+		//an den Konstruktor werden der oberste Knoten und die Größe übergeben
+		Scene meineScene = new Scene(root, 550, 502);
+		//den Titel über stage setzen
+		meineStage.setTitle("Cesar-VeniVidiViciV1.0");
+		//die Szene setzen
+		meineStage.setScene(meineScene);
+		//und anzeigen
+		meineStage.show();
+
+	}
+
+	public static void main(String[] args) {
+		//der Start
+		launch(args);
+	}
+}
